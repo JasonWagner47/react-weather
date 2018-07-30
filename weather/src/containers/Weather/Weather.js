c829f002693956333ccd81dad38123ccb21c6cf1
Jason Wagner <wagcodes@gmail.com>
2018-07-30 15:32:46 -0600

    Setting up placeholders

---
+++ weather/src/containers/Weather/Weather.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import axios from '../../axios';
+import Card from '../../components/Card/Card';
+import NewCard from '../../components/NewCard/NewCard';
+import './Weather.css';
