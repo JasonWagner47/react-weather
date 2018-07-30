7a3eb5fa94b5c898ff4cc5f9c176d288cede7122
Jason Wagner <wagcodes@gmail.com>
2018-07-30 15:33:48 -0600

    scaffolding

---
+++ weather/src/components/Card/Card.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Card extends Component {
+
+}
+
+export default Card;
---
+++ weather/src/components/NewCard/NewCard.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class NewCard extends Component {
+
+}
+
+export default NewCard;
