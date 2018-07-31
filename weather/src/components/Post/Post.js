71b10b1cbe20e6cfc3646a33bc26fbe0764a44e8
Jason Wagner <wagcodes@gmail.com>
2018-07-31 10:21:48 -0600

    Outputting current hardcoded weather

---
+++ weather/src/components/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const post = (props) => (
+   
+     <article>
+        <h1>Curret Temp: {props.current}</h1>
+        <h1>High: {props.high} </h1>
+        <h1>Low: {props.low}</h1>
+    </article>
+);
+
+export default post;
--- weather/src/containers/Weather/Weather.js
+++ weather/src/containers/Weather/Weather.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
-import Card from '../../components/Card/Card';
-// import NewCard from '../../components/NewCard/NewCard';
-// import './Weather.css';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+
+const city = 'Denver';
+const country ='USA';
+const ApiKey = '1c198b3372d8bdb9aa90f8308847b060';
 
 class Weather extends Component {
     state = {
+        posts: [],
         error: false
     }
-
+    //lifecycle hook
     componentDidMount () {
-       
+        axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' + country + '&appid=' + ApiKey)
+      
+        //promise
+        .then(response => {
+           
+            //todo ... add limit to response here with slice after conversion
+            const posts = response.data.list.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                }
+            });
+
+            console.log(updatedPosts);
+          
+            this.setState({posts: updatedPosts});
+                //do something
+            } )
+            .catch(error => {
+                console.log('[this is the error] ' + error);
+                this.setState({error: true});
+            });
     }
-    
+
     render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                key={post.dt} 
+                current={post.main.temp}
+                high={post.main.temp_max} 
+                low={post.main.temp_min}/>;
+            });
+    
+        }
+        //not a fan of the obligatory div, but C'est la vie
+
         return (
-            <div> Yo
-                <Card />
+            <div>
+              {posts}
             </div>
+     
         );
     }
+
 }
 
 export default Weather;
