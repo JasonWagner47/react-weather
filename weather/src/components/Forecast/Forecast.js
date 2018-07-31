a2b06a678b32190d3af7f477767cfcea83e8b564
Jason Wagner <wagcodes@gmail.com>
2018-07-31 15:42:20 -0600

    adding forecast component

---
+++ weather/src/components/Forecast/Forecast.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const forecast = (props) => (
+
+
+     <article>
+         <h1>Day: {props.day}</h1>
+        <h1>Dooms Day: {props.doomsday}</h1>
+        <h1>High: {props.high} </h1>
+        <h1>Low: {props.low}</h1>
+        <h1>Conditions {props.conditions}</h1>
+        <img className="image" src={`http://openweathermap.org/img/w/${props.icon}.png`} alt="weather icon" />
+    </article>
+);
+
+export default forecast;
