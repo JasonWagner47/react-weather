efce224e39a0b624aa7c699d22e61dab83dd60d8
Jason Wagner <wagcodes@gmail.com>
2018-08-08 15:18:46 -0600

    correcting defaultValue

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,7 +6,7 @@ const search = (props) => (
       <label className="d-block city-label" htmlFor="city">
         Please Type a City Below
       </label>
-      <input type="text" className="form-control mb-2" name="city" placeholder="City" defaultValue="Denver" />
+      <input type="text" className="form-control mb-2" name="city" placeholder="Denver" />
       <button type="submit" id="btnClick" className="btn btn-primary my-2">
         {' '}
         Get Weather{' '}
