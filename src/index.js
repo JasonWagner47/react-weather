d978fc82c32a0582267c240725c4b603917c4eb9
Jason Wagner <jason.wagner@wipro.com>
2018-08-21 10:37:45 -0600

    Commenting out the steps to inject Redux into the React project

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore} from 'redux';
 import reducer from './stores.reducer';
+import { Provider } from 'react-redux;'
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
 
-//Where we create Redux store
-//The actual store is in its own folder
+// Where we create Redux store
+// The actual store is in its own folder
+// npm instal --save react-redux
+// We wrap Provider from react-redux around the app. Provider is a helper component that helps us inject store into components
+
+//Steps to connect store to react 
+//1. Store Folder with reducer method
+//2. Import Store into Index.js
+//3. Insalll react-redux package with node 
+//4. Import the Provider Helper Component 
+//5. Wrap Provider around the <App/> and pass store={store}
+//6. Write const store = createStore and pass the reducer
