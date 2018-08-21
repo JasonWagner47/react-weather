cec0fe94c244f2e4e903171155b1bf3f4201b6f6
Jason Wagner <jason.wagner@wipro.com>
2018-08-21 10:29:59 -0600

    adding the reducer file

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+
+//using counter state for example only
+
+const intitialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer; 
