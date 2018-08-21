a80aaa409af4cb6ac17565f70b02603e0073b4fa
Jason Wagner <jason.wagner@wipro.com>
2018-08-21 10:29:43 -0600

    adding store and reducer to the weather app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore} from 'redux';
+import reducer from './stores.reducer';
 
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+//Where we create Redux store
+//The actual store is in its own folder
